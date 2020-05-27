70d6f48b36861944ace984aa959572518b8fb735
test <test@ws01>
2020-05-27 11:45:42 +0300

    KarmaSheeting4
    file

---
+++ ex4/src/ex4/ex4.java
@@ -0,0 +1,15 @@
+package ex4;
+
+public class ex4 {
+	public static void main (String[] args){
+		int a = 1;
+		int b = 2;
+		System.out.println("a = " + a +"\nb = " + b +"\nМеняем переменные местами!");
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System. out. println("a = " + a +"\nb = " + b +"\nLMFAO!!!");
+
+}
+}
+
